e8b4a9aef398a5ebebae5fe8076d45ab1917ea7d
RISMME <python@istruly.sexy>
2018-06-17 23:19:54 +0900

    DELETE: point rule model

--- Server/app/models/point.py
+++ Server/app/models/point.py
@@ -4,36 +4,6 @@ from datetime import datetime
 from mongoengine import *
 
 
-class PointRuleModel(Document):
-    """
-    상벌점 규칙
-    """
-    meta = {
-        'collection': 'point_rule'
-    }
-
-    name = StringField(
-        required=True
-    )
-    # 규칙 이름
-
-    point_type = BooleanField(
-        required=True
-    )
-    # True : 상점, False : 벌점
-
-    min_point = IntField(
-        required=True
-    )
-    # 최소 점수
-
-    max_point = IntField(
-        required=True
-    )
-    # 최대 점수
-    # point_type이 false(벌점)라면 해당 필드들도 음수
-
-
 class PointHistoryModel(EmbeddedDocument):
     """
     각 학생에게 속해 있는(Embedded) 상벌점 내역
